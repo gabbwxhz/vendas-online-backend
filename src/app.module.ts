e09012213109a74ecf7d49423ff4719b9afba8ad
gabbwhxz <vnvsgabe@gmail.com>
2025-01-15 13:53:42 -0300

    criado modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,9 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { StateModule } from './state/state.module';
+import { CityModule } from './city/city.module';
+import { AddressModule } from './address/address.module';
 
 @Module({
   imports: [
@@ -27,6 +30,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       migrationsRun: true,
     }),
     UserModule,
+    StateModule,
+    CityModule,
+    AddressModule,
   ],
   controllers: [],
   providers: [],
