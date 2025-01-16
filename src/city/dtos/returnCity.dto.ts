d6b23686e2eb72c14504477228ea3a829ec15572
gabbwhxz <vnvsgabe@gmail.com>
2025-01-16 16:18:04 -0300

    criado returnCityDto

---
+++ src/city/dtos/returnCity.dto.ts
@@ -0,0 +1,15 @@
+import { ReturnStateDto } from 'src/state/dtos/returnState.dto';
+import { CityEntity } from '../entities/city.entity';
+
+export class ReturnCityDto {
+  name: string;
+  state?: ReturnStateDto;
+
+  constructor(cityEntity: CityEntity) {
+    this.name = cityEntity.name;
+
+    this.state = cityEntity.state
+      ? new ReturnStateDto(cityEntity.state)
+      : undefined;
+  }
+}
